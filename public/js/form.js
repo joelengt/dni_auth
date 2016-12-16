ffa55e278cdf9c45f1ce5ef47c32fdf5317d61b8
joelengt <joelengt@gmail.com>
2016-12-16 15:28:20 -0500

    Update sent to Form by promises

--- public/js/form.js
+++ public/js/form.js
@@ -1,48 +1,51 @@
-console.log('Lectura de esta zona');
+(function () {
 
-// Date from Form input
-var $formMain = document.querySelector('#FormMain');
-var $URL_DATA = $formMain.dataset.url;
-var $boxInfo = document.querySelector('#boxInfo')
+function formEvent() {
 
-// Form DOM Elements
-var $btnToSend = document.querySelector('#btnSendForm');
-var $txtCodigo = document.querySelector('#txtCodigo');
-var $txtDni = document.querySelector('#txtDni');
+    // Date from Form input
+    var $formMain = document.querySelector('#FormMain');
+    var $URL_DATA = $formMain.dataset.url;
+    var $boxInfo = document.querySelector('#boxInfo')
 
+    // Form DOM Elements
+    var $btnToSend = document.querySelector('#btnSendForm');
+    var $txtCodigo = document.querySelector('#txtCodigo');
+    var $txtDni = document.querySelector('#txtDni');
 
-// Evento click btnToSend
-$btnToSend.addEventListener('click', function (e) {
-    // input data partner
-    var infoPartner = {
-        codigo: $txtCodigo.value,
-        dni: $txtDni.value
+    // Promise to Partner
+    function fromToSend(reqURL, info, method) {
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                url: reqURL,
+                method: method,
+                data: info,
+                success: function (result) {
+                    resolve(result)
+                },
+                statusCode: {
+                    422: function(err) {
+                        reject(err)
+                    }
+                }
+            })
+        })
     }
 
-    console.log('Evento Click');
+    // Evento click btnToSend
+    $btnToSend.addEventListener('click', function (e) {
+        // input data partner
+        var infoPartner = {
+            codigo: $txtCodigo.value,
+            dni: $txtDni.value
+        }
 
-    fromToSend($URL_DATA, infoPartner, 'post', function (result) {
-        console.log('DATOS');
-        console.log(result);
-
-        document.body.innerHTML = result;
-
-    });
-
-    return false;
-})
-
-// Form partner Data
-function fromToSend(reqURL, info, method, cb) {
-    $.ajax({
-         url: reqURL,
-         method: method,
-         data: info,
-         success: function (result) {
-             cb(result)
-         },
-         statusCode: {
-            422: function(err) {
+        // Evaluate Event from ajax
+        fromToSend($URL_DATA, infoPartner, 'post')
+            .then(function (result) {
+                console.log(result);
+                document.body.innerHTML = result;
+            })
+            .catch(function (err) {
                 // Limpiando boxInfo
                 $boxInfo.innerHTML = '';
                 console.log( "error" );
@@ -55,44 +58,19 @@ function fromToSend(reqURL, info, method, cb) {
                     $boxInfo.innerHTML += messageError + '<br>';
                     console.log(messageError);
                 }
-            }
-        }
-     })
+            })
+
+
+    })
+
 }
 
-function fromToSend33(reqURL, info, method, cb) {
-    return new Promise(function (resolve, reject) {
-        $.ajax({
-            url: reqURL,
-            method: method,
-            data: info,
-            success: function (result) {
-                cb(result)
-            }
-        })
-
-        readFile(filename, { enconding: 'utf8' }, function (err, data) {
-            if(err) {
-
-                reject(err)
-
-            } else {
-                resolve(data)
-            }
-        })
-    })
+function Main() {
+    console.log('H')
+    formEvent()
 }
 
-// usanod la promesa
+window.onload = Main()
 
-/*readFilePrimisifec(fileName)
-    .then(function (text) {
-        console.log(text)
-    })
-    .catch(function (err) {
-        console.log(err);
-    })*/
-
-
-
+})();
 
